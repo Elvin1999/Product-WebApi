cf8e738a4da4290bc6440d4a864329cbf64f836a
camalzade_elvin <camalzade_elvin@outlook.com>
2020-01-16 12:52:44 -0800

    Get,Post,Put methods are ready . . .

--- WebApiDemo/Controllers/ProductsController.cs
+++ WebApiDemo/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApiDemo.DataAccess;
+using WebApiDemo.Entities;
 
 namespace WebApiDemo.Controllers
 {
@@ -22,6 +23,52 @@ namespace WebApiDemo.Controllers
         {
             var products = _productDal.GetList();
             return Ok(products);
+        }
+        [HttpGet("{productId}")]
+        public IActionResult Get(int productId)
+        {
+            try
+            {
+            var product = _productDal.Get(p=>p.ProductId==productId);
+                if (product == null)
+                {
+                    return NotFound($"There is no product with this Id : {productId}");
+                }
+            return Ok(product);
+
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest();
+        }
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            try
+            {
+                _productDal.Add(product);
+                return new StatusCodeResult(201);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return BadRequest();
+        }
+        [HttpPut]
+        public IActionResult Put(Product product)
+        {
+            try
+            {
+                _productDal.Update(product);
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return BadRequest();
         }
     }
 }
