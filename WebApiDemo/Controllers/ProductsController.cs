b9b5002c8860ccad0bca84a414ded98af3300e72
camalzade_elvin <camalzade_elvin@outlook.com>
2020-01-16 12:10:17 -0800

    Add project files.

---
+++ WebApiDemo/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiDemo.DataAccess;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController:Controller
+    {
+        IProductDal _productDal;
+
+        public ProductsController(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            var products = _productDal.GetList();
+            return Ok(products);
+        }
+    }
+}
---
+++ WebApiDemo/DataAccess/EfProductDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WebApiDemo.Entities;
+
+namespace WebApiDemo.DataAccess
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthWindContext>,IProductDal
+    {
+        
+    }
+}
